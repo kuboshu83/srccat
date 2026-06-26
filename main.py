e3fa500fb65db8f12d46d69f0c38a25e61b3fad8
kuboshu83 <dot.sohkubo@gmail.com>
2026-06-26 22:13:46 +0900

    コードを修正

--- main.py
+++ main.py
@@ -1,30 +1,96 @@
-from collections.abc import Iterator
+from collections.abc import Iterator, Sequence
 from typing import Callable
 from pathlib import Path
+from dataclasses import dataclass
 import os
 import re
 
-EXCLUDE_DIR = [".venv"]
+EXCLUDE_DIR = [".venv", "venv"]
+
+
+@dataclass(frozen=True)
+class SrcFile:
+    filepath: str
+    code: str
+
+    def __post_init__(self):
+        if self.filepath == "":
+            raise ValueError("file name is blank")
+        if self.code == "":
+            raise ValueError(f"code is blank: {self.filepath}")
+
+
+@dataclass(frozen=True)
+class Header:
+    language: str
+    language_version: str | None = None
+
+
+def _cat_srcfiles(header: Header, files: Sequence[SrcFile]) -> str:
+    header_block = _create_header_block(header)
+    filelist_block = _create_filelist_block(files)
+    code_block = _create_code_blocks(files)
+    return f"""
+{header_block}
+
+{filelist_block}
+
+{code_block}
+"""
+
+
+def _create_header_block(header: Header) -> str:
+    return f"""
+++++ 言語情報
+language={header.language}
+version={header.language_version}
+"""
+
+
+def _create_filelist_block(files: Sequence[SrcFile]) -> str:
+    lines: list[str] = ["++++ ファイル一覧"]
+    for path in files:
+        lines.append(f"filepath={path.filepath}")
+    return "\n".join(lines)
+
+
+def _create_code_blocks(files: Sequence[SrcFile]) -> str:
+    blocks: list[str] = []
+    for file in files:
+        block = _create_file_block(file)
+        blocks.append(block)
+    return "\n".join(blocks)
+
+
+def _create_file_block(file: SrcFile) -> str:
+    return f"""
+++++ ソースコード情報
+filepath={file.filepath}
+code=```
+{file.code}
+```
+"""
 
 
 def main(language: str, srcdir: Path, pattern: re.Pattern[str], recursive: bool):
-    text = _cat_srcfiles(language, srcdir, pattern, recursive, _search_srcfiles)
+    text = cat_srcfiles(language, srcdir, pattern, recursive, _search_srcfiles)
     print(text)
 
 
-def _cat_srcfiles(
+def cat_srcfiles(
     language: str,
     srcdir: Path,
     pattern: re.Pattern[str],
     recursive: bool,
     collect_files: Callable[[Path, re.Pattern[str], bool], Iterator[Path]],
 ) -> str:
-    lines: list[str] = []
-    lines.append(_create_language_info_block(language))
-    for srcfile in collect_files(srcdir, pattern, recursive):
-        srcfile_block = _create_srcfile_block(srcfile)
-        lines.append(srcfile_block)
-    return "\n".join(lines)
+    header = Header(language)
+    srcfiles: list[SrcFile] = []
+    for path in collect_files(srcdir, pattern, recursive):
+        srcfiles.append(
+            SrcFile(str(path), path.read_text(encoding="utf-8", errors="strict"))
+        )
+    return _cat_srcfiles(header, srcfiles)
 
 
 def _search_srcfiles(
@@ -58,26 +124,6 @@ def _scan_dir(srcdir: str, recursive: bool) -> Iterator[os.DirEntry[str]]:
                         continue
         except (FileNotFoundError, PermissionError):
             continue
-
-
-def _create_language_info_block(language: str) -> str:
-    return f"""
-++++ 使用する言語の情報 ++++
-language: {language}
-
-"""
-
-
-def _create_srcfile_block(src: Path) -> str:
-    abs_path = src.resolve()
-    src_code = src.read_text()
-    return f"""
-++++ ソースコード情報
-filepath = {abs_path}
-code = ```
-{src_code}
-```
-"""
 
 
 if __name__ == "__main__":
