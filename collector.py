57d2a51ecd008de9006ff47320a32a5315f54080
kuboshu83 <dot.sohkubo@gmail.com>
2026-06-27 16:19:19 +0900

    コード整理

---
+++ collector.py
@@ -0,0 +1,71 @@
+from pathlib import Path
+from collections.abc import Iterator
+from logging import Logger
+import re
+import os
+
+_EXCLUDE_DIR = [".venv", "venv"]
+
+
+class FileCollector:
+    def __init__(
+        self,
+        srcdir: Path,
+        pattern: re.Pattern[str],
+        recursive: bool,
+        logger: Logger | None = None,
+    ):
+        self._srcdir = srcdir
+        self._pattern = pattern
+        self._recursive = recursive
+        self._logger = logger
+
+    def collect_target_files(self) -> Iterator[Path]:
+        for entry in self._collect_files(str(self._srcdir), self._recursive):
+            if not self._pattern.match(entry.name):
+                continue
+            yield Path(entry.path)
+
+    def _collect_files(
+        self, srcdir: str, recursive: bool
+    ) -> Iterator[os.DirEntry[str]]:
+        dir_stack: list[str] = [srcdir]
+        while dir_stack:
+            p = dir_stack.pop()
+            try:
+                with os.scandir(p) as it:
+                    for entry in it:
+                        try:
+                            if entry.is_file(follow_symlinks=False):
+                                yield entry
+                            elif (
+                                entry.is_dir(follow_symlinks=False)
+                                and recursive
+                                and (entry.name not in _EXCLUDE_DIR)
+                            ):
+                                dir_stack.append(entry.path)
+                        except FileNotFoundError:
+                            self._log_info(
+                                f"skip file scan: file not found: {entry.path}"
+                            )
+                            continue
+                        except PermissionError as ex:
+                            self._log_warning(f"skip file scan: {entry.path}", ex)
+                            continue
+            except FileNotFoundError:
+                self._log_info(f"skip directory scan: file not found: {p}")
+                continue
+            except PermissionError as ex:
+                self._log_warning(f"skip directory scan: {p}", ex)
+                continue
+
+    def _log_info(self, msg: str):
+        if self._logger is not None:
+            self._logger.info(msg)
+
+    def _log_warning(self, msg: str, ex: Exception):
+        if self._logger is not None:
+            self._logger.warning("%s: %s", msg, ex, exc_info=True)
+
+
+__all__ = ["FileCollector"]
--- main.py
+++ main.py
@@ -1,38 +1,31 @@
-from collections.abc import Iterator
-from typing import Callable
 from pathlib import Path
 import re
 
 import model
 import render
-import filecollector
+import collector
 
 
-def main(language: str, srcdir: Path, pattern: re.Pattern[str], recursive: bool):
-    text = cat_srcfiles(
-        language, srcdir, pattern, recursive, filecollector.search_srcfiles
-    )
+def main(language: model.Language, collector: collector.FileCollector):
+    text = cat_srcfiles(language, collector)
     print(text)
 
 
 def cat_srcfiles(
-    language: str,
-    srcdir: Path,
-    pattern: re.Pattern[str],
-    recursive: bool,
-    collect_files: Callable[[Path, re.Pattern[str], bool], Iterator[Path]],
+    language: model.Language,
+    collector: collector.FileCollector,
 ) -> str:
-    lang = model.Language.from_str(language)
     srcfiles: list[model.SrcFile] = []
-    for path in collect_files(srcdir, pattern, recursive):
+    for path in collector.collect_target_files():
         srcfiles.append(
             model.SrcFile(str(path), path.read_text(encoding="utf-8", errors="strict"))
         )
-    return render.render_review_document(lang, srcfiles)
+    return render.render_review_document(language, srcfiles)
 
 
 if __name__ == "__main__":
     language = model.Language.from_str("Python")
     srcdir = Path(".")
     pattern = re.compile(r"^.+\.py$")
-    main(language.value.display_name, srcdir, pattern, True)
+    file_collector = collector.FileCollector(srcdir, pattern, True, None)
+    main(language, file_collector)
