e590d73abed36e2953aa6c802e50bf3c251a5a46
kuboshu83 <dot.sohkubo@gmail.com>
2026-06-29 23:24:08 +0900

    責務分離

--- src/srccat/config.py
+++ src/srccat/config.py
@@ -2,9 +2,48 @@ import argparse
 import re
 from pathlib import Path
 from dataclasses import dataclass
+from collections.abc import Sequence
 
-import srccat.model
-import srccat.errors
+import srccat.model as model
+import srccat.errors as errors
+
+
+def _convert_string_to_scan_root_dir(dir_path: str) -> Path:
+    path = Path(dir_path)
+    if not path.is_dir():
+        raise errors.InvalidConfigError(
+            f"scan root directory is not directory: {dir_path}"
+        )
+    return path
+
+
+def _convert_string_to_language(language_str: str) -> model.Language:
+    try:
+        return model.Language.from_str(language_str)
+    except errors.InvalidArgumentError as ex:
+        raise errors.InvalidConfigError(f"language conversion error") from ex
+
+
+def _convert_string_to_encoding(encoding_str: str) -> model.Encoding:
+    try:
+        return model.Encoding.from_str(encoding_str)
+    except errors.InvalidArgumentError as ex:
+        raise errors.InvalidConfigError(f"encoding conversion error") from ex
+
+
+def _convert_string_to_regex_pattern(
+    pattern_strings: Sequence[str],
+) -> Sequence[re.Pattern[str]]:
+    patterns: list[re.Pattern[str]] = []
+
+    for pattern_str in pattern_strings:
+        try:
+            patterns.append(re.compile(pattern_str))
+        except re.error as ex:
+            raise errors.InvalidConfigError(
+                f"invalid reject directory name regex pattern: {pattern_str}"
+            ) from ex
+    return patterns
 
 
 @dataclass(frozen=True)
@@ -13,11 +52,11 @@ class ApplicationConfig:
     外部からの取得が必要なアプリケーションで使用する設定
     """
 
-    language: srccat.model.Language
+    language: model.Language
     scan_directory_recursive: bool
     scan_root_directory: Path
     reject_dir_name_patterns: tuple[re.Pattern[str], ...]
-    source_file_encoding: srccat.model.Encoding
+    source_file_encoding: model.Encoding
     source_file_name_patterns: tuple[re.Pattern[str], ...]
 
     @classmethod
@@ -30,43 +69,11 @@ class ApplicationConfig:
         source_file_encoding: str,
         source_file_name_patterns: list[str],
     ) -> ApplicationConfig:
-        try:
-            language = srccat.model.Language.from_str(programming_language)
-        except ValueError as ex:
-            raise srccat.errors.InvalidConfigError(
-                f"invalid programming language: {programming_language}"
-            ) from ex
-
-        dirpath = Path(scan_root_directory)
-        if not dirpath.is_dir():
-            raise srccat.errors.InvalidConfigError(
-                f"scan root directory is not directory: {scan_root_directory}"
-            )
-
-        try:
-            encoding = srccat.model.Encoding.from_str(source_file_encoding)
-        except ValueError as ex:
-            raise srccat.errors.InvalidConfigError(
-                f"invalid encoding: {source_file_encoding}"
-            ) from ex
-
-        dir_name_patterns: list[re.Pattern[str]] = []
-        for pattern_str in reject_dir_name_patterns:
-            try:
-                dir_name_patterns.append(re.compile(pattern_str))
-            except re.error as ex:
-                raise srccat.errors.InvalidConfigError(
-                    f"invalid reject directory name regex pattern: {pattern_str}"
-                ) from ex
-
-        file_name_patterns: list[re.Pattern[str]] = []
-        for pattern in source_file_name_patterns:
-            try:
-                file_name_patterns.append(re.compile(pattern))
-            except re.error as ex:
-                raise srccat.errors.InvalidConfigError(
-                    f"invalid search file name regex pattern: {pattern}"
-                ) from ex
+        language = _convert_string_to_language(programming_language)
+        dirpath = _convert_string_to_scan_root_dir(scan_root_directory)
+        encoding = _convert_string_to_encoding(source_file_encoding)
+        dir_name_patterns = _convert_string_to_regex_pattern(reject_dir_name_patterns)
+        file_name_patterns = _convert_string_to_regex_pattern(source_file_name_patterns)
 
         return ApplicationConfig(
             language=language,
