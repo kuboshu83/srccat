4b8e905938d5b284b1344ecd3250f3356bb9764b
kuboshu83 <dot.sohkubo@gmail.com>
2026-06-30 19:50:59 +0900

    行番号を出力するようにした

--- src/srccat/main.py
+++ src/srccat/main.py
@@ -3,26 +3,35 @@ import srccat.render
 import srccat.collector
 import srccat.filefilter
 import srccat.config
+import srccat.processor as processor
 import logging
+
+# ソースコードの最大の行番号。10000行あるコードはレビュー以前にNGなので10000に設定する。
+_MAX_LINE_NO = 10000
 
 
 def run(
     language: srccat.model.Language,
     collector: srccat.collector.FilteredFileCollector,
     encoding: srccat.model.Encoding,
+    max_source_file_line_no: int,
 ) -> str:
-    return build_review_document(language, collector, encoding)
+    return build_review_document(language, collector, encoding, max_source_file_line_no)
 
 
 def build_review_document(
     language: srccat.model.Language,
     collector: srccat.collector.FilteredFileCollector,
     encoding: srccat.model.Encoding,
+    max_source_file_line_no: int,
 ) -> str:
     loaded_source_files: list[srccat.model.LoadedSourceCode] = []
     for path in collector.collect_target_files():
         try:
-            code_body = path.read_text(encoding=encoding.codec, errors="strict")
+            code_body = processor.add_line_number_to_head(
+                path.read_text(encoding=encoding.codec, errors="strict"),
+                max_source_file_line_no,
+            )
             loaded_source_files.append(
                 srccat.model.LoadedSourceCode.with_success(str(path), code_body)
             )
@@ -53,7 +62,7 @@ def main():
     )
     file_collector = srccat.collector.FilteredFileCollector(dir_scanner, filters)
     encoding = config.source_file_encoding
-    text = run(language, file_collector, encoding)
+    text = run(language, file_collector, encoding, _MAX_LINE_NO)
     print(text)
 
     error_count = file_collector.error_count
